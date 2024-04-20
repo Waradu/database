d59cd85813f4637a7195341a04ad4443f83b52b4
Waradu <waradu@outlook.com>
2024-04-20 12:48:37 +0200

    fixed width added first step for custom rows

---
+++ components/items/text.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  props: {
+    type: {
+      type: String,
+      default: "linear",
+    },
+    size: {
+      type: [String, Number],
+      default: 24,
+    },
+    color: {
+      type: String,
+      default: "currentColor",
+    },
+    thickness: {
+      type: [String, Number],
+      default: 1.5,
+    }
+  },
+})
+</script>
+
+<style>
+
+</style>
