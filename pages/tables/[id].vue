a01641b5f223222cb3f6a7fda2a8d5ff3669d6d3
Waradu <waradu@outlook.com>
2024-05-14 11:04:42 +0200

    formatted code

--- pages/tables/[id].vue
+++ pages/tables/[id].vue
@@ -4,15 +4,27 @@
       <h1>
         <div class="header-wrapper">
           <NuxtLink class="link" to="/">
-            <Iconsax name="ArrowLeft" size="18" color="#ffffff80" thickness="3" />
+            <Iconsax
+              name="ArrowLeft"
+              size="18"
+              color="#ffffff80"
+              thickness="3"
+            />
           </NuxtLink>
           <Iconsax :name="table.icon" size="28" />
         </div>
         <div class="header-text">{{ table.name }}</div>
       </h1>
       <div class="tags">
-        <div v-for="tag in databaseStore.getTableTags(table.id.toString())" :key="tag.id" class="tag"
-          :style="{ '--color': tag.color + '50', '--full-color': tag.color + '80' }">
+        <div
+          v-for="tag in databaseStore.getTableTags(table.id.toString())"
+          :key="tag.id"
+          class="tag"
+          :style="{
+            '--color': tag.color + '50',
+            '--full-color': tag.color + '80',
+          }"
+        >
           {{ tag.name }}
         </div>
       </div>
@@ -21,7 +33,11 @@
       <div class="search">
         <div class="searchbar">
           <Iconsax name="SearchNormal" size="18" color="#ffffff30" />
-          <input type="text" placeholder="Search Rows for Name or Tag" v-model="search">
+          <input
+            type="text"
+            placeholder="Search Rows for Name or Tag"
+            v-model="search"
+          />
         </div>
       </div>
       <div class="content" v-if="filteredRows.length <= 0">
@@ -30,7 +46,12 @@
           <div class="name">Nothing found</div>
         </div>
       </div>
-      <NuxtLink class="content" v-for="(row, index) in filteredRows" :key="row.id" :to="`/rows/${row.id}`">
+      <NuxtLink
+        class="content"
+        v-for="(row, index) in filteredRows"
+        :key="row.id"
+        :to="`/rows/${row.id}`"
+      >
         <div class="data">
           <div class="number">{{ index + 1 }}.</div>
           <div class="name">{{ row.name }}</div>
@@ -38,8 +59,15 @@
         </div>
         <div class="info">
           <div class="tags">
-            <div v-for="tag in databaseStore.getRowTags(row.id.toString())" :key="tag.id" class="tag"
-              :style="{ '--color': tag.color + '50', '--full-color': tag.color + '80' }">
+            <div
+              v-for="tag in databaseStore.getRowTags(row.id.toString())"
+              :key="tag.id"
+              class="tag"
+              :style="{
+                '--color': tag.color + '50',
+                '--full-color': tag.color + '80',
+              }"
+            >
               {{ tag.name }}
             </div>
           </div>
@@ -56,8 +84,14 @@ const databaseStore = useDatabaseStore();
 
 const tableId = computed(() => route.params.id as string);
 const table = computed(() => {
-  const data = databaseStore.getTable(tableId.value)
-  if (!data) throw new Error('Table not found');
+  const data = databaseStore.getTable(tableId.value);
+  if (!data) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "Row Not Found",
+      fatal: true,
+    });
+  }
   return data;
 });
 const filteredRows = computed(() => {
@@ -65,24 +99,42 @@ const filteredRows = computed(() => {
   if (!searchText) {
     return databaseStore.getTableRows(tableId.value);
   }
-  return databaseStore.getTableRows(tableId.value).filter(row =>
-    row.name.toLowerCase().includes(searchText) ||
-    databaseStore.getRowTags(row.id.toString()).some(tag => tag.name.toLowerCase().includes(searchText))
-  );
+  return databaseStore
+    .getTableRows(tableId.value)
+    .filter(
+      (row) =>
+        row.name.toLowerCase().includes(searchText) ||
+        databaseStore
+          .getRowTags(row.id.toString())
+          .some((tag) => tag.name.toLowerCase().includes(searchText))
+    );
 });
 
 function formatDate(dateStr: string) {
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    const date = new Date(dateStr);
-    const day = date.getDate();
-    const month = months[date.getMonth()];
-    const year = `'${date.getFullYear().toString().slice(2)}`;
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const date = new Date(dateStr);
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = `'${date.getFullYear().toString().slice(2)}`;
 
-    return `${month} ${day} ${year}`;
+  return `${month} ${day} ${year}`;
 }
 
 useHead({
-  title: `Table: ${table.value?.name || 'Loading...'}`
+  title: `Table: ${table.value?.name || "Loading..."}`,
 });
 </script>
 
